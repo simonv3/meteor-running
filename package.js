94f849b45a9ce20d3924cf7d880ef85d83f21da9
Simon <svansintjan@gmail.com>
2016-01-16 16:36:11 -0800

    Bump to 0.4.3 and better check for Sandstorm

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "simonv3:meteor-running",
   summary: "Add sensible authentication patterns and an admin panel to your app.",
-  version: "0.4.2",
+  version: "0.4.3",
   git: "https://github.com/simonv3/meteor-running.git"
 });
 
@@ -15,8 +15,8 @@ Package.onUse(function (api) {
   api.use('angular@1.3.2', 'client');
 
   api.use('simonv3:meteor-running-models@0.0.2', ['client', 'server']);
-  api.use('simonv3:meteor-running-auth@0.4.1', ['client', 'server']);
-  api.use('simonv3:meteor-running-admin@0.4.0', 'client');
+  api.use('simonv3:meteor-running-auth@0.4.3', ['client', 'server']);
+  api.use('simonv3:meteor-running-admin@0.4.3', 'client');
 
   api.addFiles('meteor-running.js', 'client');
 
--- server/config/methods.js
+++ server/config/methods.js
@@ -1,32 +1,31 @@
 // In your server code: define a method that the client can call
 
 Meteor.startup(function() {
-  Meteor.methods({
-    isSetUp: function() {
-      var sites = Sites.find({has_been_set_up: true}).fetch()
-      if (sites.length > 0)
-        return true;
-      Meteor.call('isAdmin', function(err, resp) {
-        if (resp) {
-          Sites.update({has_been_set_up: false}, {$set: {has_been_set_up: true}})
-          return true;
-        }
-        return false;
-      })
 
-    },
+  Meteor.methods({
 
     isSandstorm: function() {
-      var user = Meteor.users.find({'services.sandstorm': {$exists: true, $ne: null}}).fetch()[0]
-      if (user.services.sandstorm)
-        return true;
-      return false;
+      return Meteor.settings.public.isInSandstorm;
     },
 
-    isSiteSetUp: function() {
+    isSetUp: function() {
+      if (Meteor.settings.public.isInSandstorm) {
+
+        if (Meteor.user()) {
+          var sandstorm = Meteor.user().services.sandstorm;
+          if (sandstorm && sandstorm.permissions[0] === 'owner') {
+            Meteor.users.update(Meteor.user()._id, {
+              'is_admin': true
+            });
+          }
+        }
+
+        return true;
+
+      }
       var users = Meteor.users.find({is_admin: true}).fetch()
       if (users.length === 0) {
-        return false
+        return false;
       } else {
         var sites = Sites.find({has_been_set_up: true}).fetch()
 
